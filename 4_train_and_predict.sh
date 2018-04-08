c2670070086df6420180c8c54b676881163e602a
Ronnie Wang <geniusxiaoguai@gmail.com>
2018-04-08 22:31:10 +0800

    Add more scripts

--- 4_train_and_predict.sh
+++ 4_train_and_predict.sh
@@ -1,2 +1,4 @@
 lightgbm config=lightgbm.train.config train=/home/ike/Data/TalkingData/test/train_ready.csv output_model=/home/ike/Data/TalkingData/test/lightgbm.model
 lightgbm config=lightgbm.test.config data=/home/ike/Data/TalkingData/test/test_ready.csv output_result=/home/ike/Data/TalkingData/test/test_result.csv
+sed -i '1i is_attributed' /home/ike/Data/TalkingData/test/test_result.csv
+paste -d"," /home/ike/Data/TalkingData/test/test.csv /home/ike/Data/TalkingData/test/test_result.csv | cut -d"," -f1,10 > /home/ike/Data/TalkingData/result_040702.csv
